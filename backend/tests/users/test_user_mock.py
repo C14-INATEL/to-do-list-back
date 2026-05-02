402f22d477916bc8d4c2756c6bee605d32c63799
Bianca Souza <biancaribdesouza@gmail.com>
2026-05-02 20:57:32 -0300

    test(user): adiciona testes mock para create_user e get_user_by_id

--- backend/tests/users/test_user_mock.py
+++ backend/tests/users/test_user_mock.py
@@ -7,6 +7,8 @@ from rest_framework import status
 from app.services.user_service import create_user
 from users.views import UserDetailView
 
+from app.services.user_service import get_user_by_id
+
 
 class TestUserWithMock:
     def test_create_user_raises_exception_when_email_already_exists(self):
@@ -42,3 +44,42 @@ class TestUserWithMock:
             mock_serializer.save.assert_not_called()
             assert response.status_code == status.HTTP_400_BAD_REQUEST
             assert response.data == {"email": ["Enter a valid email address."]}
+
+    def test_create_user_successfully_creates_user():
+        with patch("app.services.user_service.User.objects.filter") as mock_filter, \
+            patch("app.services.user_service.User.objects.create_user") as mock_create:
+
+            # Simula que email NÃO existe
+            mock_filter.return_value.exists.return_value = False
+
+            # Usuário fake
+            mock_user = Mock()
+            mock_create.return_value = mock_user
+
+            result = create_user("Bianca", "bianca@email.com", "123456")
+
+            # Verifica se checou o email
+            mock_filter.assert_called_once_with(email="bianca@email.com")
+
+            # Verifica se tentou criar corretamente
+            mock_create.assert_called_once_with(
+                username="bianca@email.com",
+                email="bianca@email.com",
+                first_name="Bianca",
+                password="123456"
+            )
+
+            # Verifica retorno
+            assert result == mock_user
+
+    def test_get_user_by_id_returns_user_when_found():
+        with patch("app.services.user_service.User.objects.get") as mock_get:
+
+            mock_user = Mock(id=1, email="bianca@email.com")
+            mock_get.return_value = mock_user
+
+            result = get_user_by_id(1)
+
+            mock_get.assert_called_once_with(id=1)
+
+            assert result == mock_user
